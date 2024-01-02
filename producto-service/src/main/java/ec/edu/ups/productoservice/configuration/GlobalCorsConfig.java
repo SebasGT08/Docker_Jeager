93017a6e4c290b919ca37b7236f563e143bf09d5
SebasGT08 <guaman.sebastian802@gmail.com>
2024-01-01 23:31:47 -0500

    CORS

---
+++ producto-service/src/main/java/ec/edu/ups/productoservice/configuration/GlobalCorsConfig.java
@@ -0,0 +1,18 @@
+package ec.edu.ups.productoservice.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class GlobalCorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Permite solicitudes de cualquier origen
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Personaliza según tus necesidades
+                .allowedHeaders("*") // Permite cualquier encabezado
+                .maxAge(3600); // Tiempo en segundos que el navegador debe cachear los resultados de la verificación CORS
+    }
+}
